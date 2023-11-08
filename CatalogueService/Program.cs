6622e8950dc4a9a89c1a19eed340a674fde6f984
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-09 00:20:38 +0300

    registered auto mapper for DI and suppressing C# async route masking

--- CatalogueService/Program.cs
+++ CatalogueService/Program.cs
@@ -2,11 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
