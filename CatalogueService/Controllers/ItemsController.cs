d70ce1472c570ec178f1ef0caa92441ed89ef840
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-08 03:10:05 +0300

    Add project files.

---
+++ CatalogueService/Controllers/ItemsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogueService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+    }
+}
