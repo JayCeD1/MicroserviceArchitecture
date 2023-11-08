95b728cc8971c86eee70dd1de0e77d868fa2d96f
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-09 00:26:01 +0300

    register mongo Db serializer for Guid and DateTime

--- CatalogueService/Program.cs
+++ CatalogueService/Program.cs
@@ -1,15 +1,22 @@
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers(options =>
 {
+    //Suppress Async In Route Names
     options.SuppressAsyncSuffixInActionNames = false;
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Register mongo DB Serializers
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 
 //AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
