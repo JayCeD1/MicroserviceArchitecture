e5fe81198adeb0b97a492cfb025d26e5512f81b0
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-08 10:59:11 +0300

    sample endpoint methods with static data

--- CatalogueService/Controllers/ItemsController.cs
+++ CatalogueService/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using static CatalogueService.Dtos;
 
 namespace CatalogueService.Controllers
 {
@@ -6,5 +7,22 @@ namespace CatalogueService.Controllers
     [Route("[controller]")]
     public class ItemsController : ControllerBase
     {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Portion", "Restores", 5 , DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Portion 2", "Restores all", 20 , DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        public ItemDto GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            return item;
+        }
     }
 }
