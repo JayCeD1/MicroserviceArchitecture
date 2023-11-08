bc94becfbf6582ad98d13832d45ef69ba27b942f
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-08 23:14:45 +0300

    Added auto mapper

---
+++ CatalogueService/Profiles/ItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using CatalogueService.Entities;
+using static CatalogueService.Dtos;
+
+namespace CatalogueService.Profiles
+{
+    public class ItemProfile : AutoMapper.Profile
+    {
+        public ItemProfile()
+        {
+            //Source -> Target
+            CreateMap<Item, ItemDto>();
+        }
+    }
+}
