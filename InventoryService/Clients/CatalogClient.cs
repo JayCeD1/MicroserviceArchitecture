aa0c1dcd444d4ae2fe45679440474a2e186ff8a0
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-09 20:08:29 +0300

    Added new http catalog client

---
+++ InventoryService/Clients/CatalogClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Json;
+using static InventoryService.Dtos;
+
+namespace InventoryService.Clients
+{
+    public class CatalogClient
+    {
+        private readonly HttpClient httpClient;
+
+        public CatalogClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IReadOnlyCollection<CatalogItemDto>> GetCatalogItemsAsync()
+        {
+            var items = await httpClient.GetFromJsonAsync<IReadOnlyCollection<CatalogItemDto>>("/items");
+
+            return items;
+        }
+    }
+}
