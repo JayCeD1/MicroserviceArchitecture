9c690c4acbe96d9ef39ccf0f37e9b619450efde6
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-09 20:07:44 +0300

    added new dto

--- InventoryService/Dtos.cs
+++ InventoryService/Dtos.cs
@@ -4,6 +4,8 @@
     {
         public record GrantItemsDto(Guid UserId, Guid CatalogueItemId, int Quantity);
 
-        public record InventoryItemDto(Guid CatalogueItemId, int Quantity, DateTimeOffset AcquiredDate);
+        public record InventoryItemDto(Guid CatalogueItemId, string Name, string Description, int Quantity, DateTimeOffset AcquiredDate);
+
+        public record CatalogItemDto(Guid Id, string Name, string Description);
     }
 }
