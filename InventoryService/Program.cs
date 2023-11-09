c2294a34ea4428251da7abd0dada0c08baf2c4ba
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-09 17:19:28 +0300

    configured dependency services

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -1,3 +1,6 @@
+using Common.MongoDB;
+using InventoryService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add Mongo Configuration
+builder.Services.AddMongo()
+    .AddMongoRepo<InventoryItem>("inventoryitems");
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
