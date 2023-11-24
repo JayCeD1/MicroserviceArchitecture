bd4525891d153d1efd99939fb9578710bb6c8c75
JayCeD1 <jesseeren@gmail.com>
2023-11-24 23:24:46 +0300

    refactoring and adding mass transit config

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -4,6 +4,7 @@ using InventoryService.Entities;
 using Polly;
 using Polly.Timeout;
 using System;
+using Common.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,39 +17,12 @@ builder.Services.AddSwaggerGen();
 
 //Add Mongo Configuration
 builder.Services.AddMongo()
-    .AddMongoRepo<InventoryItem>("inventoryitems");
+    .AddMongoRepo<InventoryItem>("inventoryitems")
+    .AddMongoRepo<CatalogueItem>("catalogueitems")
+    .AddMassTransitWithRabbitMq();
 
 
-//Http client with jitter config
-Random jitter = new Random();
-
-builder.Services.AddHttpClient<CatalogClient>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7212");
-}).AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-    5, 
-    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitter.Next(0,1000)),
-    onRetry: (outcome, timespan, retryAttempt) =>
-    {
-        var serviceProvider = builder.Services.BuildServiceProvider();
-        serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
-    }
-))//Circuit breaker pattern
-.AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().CircuitBreakerAsync(
-    3, TimeSpan.FromTicks(15),
-    onBreak: (outcome, timespan) =>
-    {
-        var serviceProvider = builder.Services.BuildServiceProvider();
-        serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Opening circuit for {timespan.TotalSeconds} seconds...");
-    },
-    onReset: () =>
-    {
-        var serviceProvider = builder.Services.BuildServiceProvider();
-        serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Closing circuit ...");
-    }
-    ))
-    //Time out handler
-    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+AddCatalogueClient(builder);
 
 //AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -69,3 +43,39 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddCatalogueClient(WebApplicationBuilder webApplicationBuilder)
+{
+//Http client with jitter config
+    Random jitter = new Random();
+
+    webApplicationBuilder.Services
+        .AddHttpClient<CatalogClient>(client => { client.BaseAddress = new Uri("https://localhost:7212"); })
+        .AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+            5,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) +
+                            TimeSpan.FromMilliseconds(jitter.Next(0, 1000)),
+            onRetry: (outcome, timespan, retryAttempt) =>
+            {
+                var serviceProvider = webApplicationBuilder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()
+                    ?.LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+            }
+        )) //Circuit breaker pattern
+        .AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+            3, TimeSpan.FromTicks(15),
+            onBreak: (outcome, timespan) =>
+            {
+                var serviceProvider = webApplicationBuilder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()
+                    ?.LogWarning($"Opening circuit for {timespan.TotalSeconds} seconds...");
+            },
+            onReset: () =>
+            {
+                var serviceProvider = webApplicationBuilder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Closing circuit ...");
+            }
+        ))
+        //Time out handler
+        .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+}
